6d15e3f98360f58653caa09d6b281eb32afa92e4
yangmulia <57234987+masbred@users.noreply.github.com>
2026-02-26 18:58:40 +0700

    Add files via upload

---
+++ sbd_mekanikkota/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'adamant'
+game 'gta5'
+
+author 'SBD_STORE'
+
+this_is_a_map "yes"
+
+files {
+    'stream/**/*.ytyp',
+    'stream/**/*.ymap',
+}
+
+data_file 'DLC_ITYP_REQUEST' 'stream/*.ytyp'
